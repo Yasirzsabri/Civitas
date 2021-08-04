c35254b02a5b24cac114e2438dd46285c19a05b3
Yasirzsabri <yasir.zsabri@gmail.com>
2021-08-04 08:08:17 -0600

    onclick calendar

--- client/src/components/screens/Calendar.js
+++ client/src/components/screens/Calendar.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react"
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin,{DateClickArg} from '@fullcalendar/interaction';
+
 
 
 
 const Calendar =()=>{
   let [events, setEvents] = useState({})
-    console.log("killme")
     let newEvents=[]
     
      const getEvent = async () => {
@@ -17,6 +18,8 @@ const Calendar =()=>{
         newEvents.push({title: '' , date: ''})
         newEvents[i].title=data[i].name
         newEvents[i].date=data[i].start
+        newEvents[i].id=data[i]._id
+        newEvents[i].index=i
         
     }
     setEvents(newEvents);
@@ -25,14 +28,22 @@ const Calendar =()=>{
     getEvent(); 
   }, []);
    
-   console.log("line25 im here",events)
+   
   
-
+ const handleDateClick = (i)=>{
+   console.log (i.event.id)
+   i.dayEl.addEventListener('dblclick',()=>{
+   alert(`im Working ${JSON.stringify(i.event.id)}`);
+     
+  })
+ }
 return(
     <FullCalendar
-  plugins={[ dayGridPlugin ]}
+
+  plugins={[ dayGridPlugin,interactionPlugin ]}
   initialView="dayGridMonth"
   events={events}
+  eventClick={handleDateClick}
   //  events={[
   //   { title: 'event 1', date: '2021-07-29' },
   //   { title: 'event 2', date: '2021-07-30' }
