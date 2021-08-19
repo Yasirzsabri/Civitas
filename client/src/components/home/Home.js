5bae6cc3708ce9a3df9c0b5c1b4b468f612a412b
ParkerSjogren <sjogrenp@telus.net>
2021-08-19 14:20:28 -0600

    Fix Event Calendar link on home bar

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -45,8 +45,10 @@ const Home = () => {
                         <ul>
                         <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
                         {/* <li><a className="nav-link scrollto" href="#about">About</a></li> */}
-                        <li><a className="nav-link scrollto" href="http://localhost:4444/calendar">Events Schedule</a></li>
-                        <li><a className="nav-link scrollto" href="http://localhost:4444/Pay">Become A Membership</a></li>
+                        {/* <li><a className="nav-link scrollto" href="http://localhost:4444/calendar">Events Schedule</a></li>
+                        <li><a className="nav-link scrollto" href="http://localhost:4444/Pay">Become A Membership</a></li> */}
+                        <li><a className="nav-link scrollto" href="/calendar">Events Schedule</a></li>
+                        <li><a className="nav-link scrollto" href="/Pay">Pay for Membership</a></li>
                         <li><a className="nav-link scrollto" href="#gallery">Gallery</a></li>
                         {/* <li><a className="nav-link scrollto" href="#contact">Contact</a></li> */}
                         </ul>
